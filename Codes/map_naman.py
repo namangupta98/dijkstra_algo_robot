bc4a5b0534aa38391c4489621f231bcb2cc21878
Naman Gupta <namanryan@gmail.com>
2020-02-28 19:48:09 -0500

    Updated map.py
    Updated map using OpenCV()

--- Codes/map_naman.py
+++ Codes/map_naman.py
@@ -20,10 +20,39 @@ def circle_obstacle(x, y, radius, w):
             if (i-x)**2 + (j-y)**2 < radius:
                 w[i][j] = 1
 
+# def obstacle(lines, xmin, xmax, ymin, ymax, w):
+#     for i in range(xmin, xmax + 1):
+#         for j in range(ymin, ymax + 1):
+#             for l in lines:
+#                 if l[3] == 0 and (l[0]*i + l[1]*j + l[2] > 0):
+#                     w[i][j] = 1
+#                 elif l[3] == 1 and (l[0]*i + l[1]*j + l[2] < 0):
+#                     w[i][j] = 1
+#
+# def getLineParam(x1, y1, x2, y2):
+#     a = float((x2-x1)/(y2-y1))
+#     b = -1
+#     c = y1 - a*x1
 
-World = world(100, 200)
-square_obstacle(40, 90, 20, World)
-circle_obstacle(50, 160, 15, World)
+
+# def minimax(x, y):
+
+
+World = world(200, 300)
+# square_obstacle(40, 90, 20, World)
+# circle_obstacle(50, 160, 15, World)
+
+# Shape-1 6 corners
+pts = np.array([[25, 185], [75, 185], [100, 150], [75, 120], [50, 150], [20, 120]], np.int32)
+pts = pts.reshape((-1, 1, 2))
+cv2.fillPoly(World, [pts], (255, 255, 255))
+
+# Shape-2 Circle
+cv2.circle(World, (225, 150), 25, (255, 255, 255), -1)
+
+# Shape-3 Ellipse
+cv2.ellipse(World, (150, 100), (40, 20),0, 0, 180, 255, thickness=-1)
+
 
 cv2.imshow("World", World)
 cv2.waitKey(0)
